c8babb904970c614e414b25a613830e4a8cf1088
Mgrsc <118801216+Mgrsc@users.noreply.github.com>
2023-11-22 13:46:16 +0800

    Update kuber.sh
    fix

--- general/kuber.sh
+++ general/kuber.sh
@@ -27,12 +27,12 @@ echo "The latest Kubernetes version is: $latest_k8s_version"
 echo -e "Which version do you want to install? Please specify:"
 read -r k8s_version
 k8s_version_short=$(echo "$k8s_version" | cut -d. -f1-2)
-sudo apt-get update
 sudo mkdir -p /etc/apt/keyrings/
 sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v${k8s_version_short}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${k8s_version_short}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 latest_k8s_response=$(curl -s https://api.github.com/repos/kubernetes/kubernetes/releases/latest)
 latest_k8s_version=$(echo "$latest_k8s_response" | jq -r '.tag_name')
+sudo apt-get update
 sudo apt-get install -y kubelet=${k8s_version}* kubeadm=${k8s_version}* kubectl=${k8s_version}*
 
 
